9231dac8bd9dc52a282bf18d4937503db93a68ff
Ayush Kumar Bansal <ayushbansal19505@gmail.com>
2026-09-05 17:45:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const hamburger = document.querySelector(".hamburger-icon");
 const crossbtn = document.querySelector(".close-icon");
+const bodyblock = document.querySelector(".open-nav")
 const mobilenav = document.querySelector(".mobile-nav");
 const navlink = document.querySelectorAll(".mobile-nav .links");
+const contactpage = document.querySelector(".contact-page");
+const contactbtn = document.querySelectorAll(".footer-btns .contact-btn")
 
 hamburger.addEventListener("click", () => {
     mobilenav.classList.add("active");
+    document.body.classList.add("open-nav");
 })
 
 crossbtn.addEventListener("click", () => {
-    mobilenav.classList.remove("active")
+    mobilenav.classList.remove("active");
+    document.body.classList.remove("open-nav");
 })
 
 navlink.forEach((link) => {
@@ -16,3 +21,10 @@ navlink.forEach((link) => {
         mobilenav.classList.remove("active");
     })
 })
+contactbtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        contactpage.classList.add("active");
+        console.log("helo");
+        
+    });
+});
